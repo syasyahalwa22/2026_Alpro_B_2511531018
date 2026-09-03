40de870d43c9ce2aeb4ff82aab34726ad3c37292
syasyahalwa22 <syasyahalwa22@gmail.com>
2026-09-03 10:28:06 +0700

    Add files via upload

---
+++ Pekan1/program1_2511531018.py
@@ -0,0 +1,14 @@
+# program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\ /// \\\\\\")
+print("program ini" + " dibuat oleh a/n"
++ "Syasya Halwa Gazwani dan 2511531018.")
+
